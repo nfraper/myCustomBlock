090183619a0665848732181f86c854fac307c22c
t034553 <nerea.frances-perez.contractor@bbva.com>
2024-11-18 16:36:36 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <CustomBlock />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CustomBlock from './components/CustomBlock.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    CustomBlock,
+  },
+});
+</script>
+
+<style>
+/* Estilos globales si son necesarios */
+</style>
---
+++ src/components/CustomBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Este es un texto plano de mi Custom Block.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CustomBlock',
+});
+</script>
+
+<style scoped>
+/* Agrega estilos aquí si es necesario */
+</style>
